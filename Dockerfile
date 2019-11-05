647cba7eab67ca6655b178a7cb75781afcc0c1af
Josh Gavant <joshgavant@gmail.com>
2019-11-05 16:27:27 -0600

    fix bugs

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM registry.redhat.io/openshift4/ose-jenkins-agent-base:latest
 USER 0:0
 
-RUN PACKAGES="openssl jq" \
+RUN PACKAGES="openssl" \
  && yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm \
  && yum -y install $PACKAGES \
  && yum clean all \
@@ -12,6 +12,11 @@ RUN ver=1.2.3 \
  && curl -fsSLO https://releases.hashicorp.com/vault/${ver}/vault_${ver}_linux_amd64.zip \
  && unzip vault_${ver}_linux_amd64.zip \
  && cp vault /usr/bin
+
+RUN ver=1.6 \
+ && curl -fsSL https://github.com/stedolan/jq/releases/download/jq-${ver}/jq-linux64 \
+         -o /usr/bin/jq \
+ && chmod 0775 /usr/bin/jq
 
 # in OpenShift the actual user/UID will be randomly assigned
 USER 1001:0
--- hack/deploy.sh
+++ hack/deploy.sh
@@ -3,11 +3,13 @@
 this_dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 root_dir=$(dirname ${this_dir})
 
-export namespace_name=default
+export build_namespace_name=${1:-vault-ns}
+export agent_namespace_name=${2:-vault-ns}
+
 export imagestream_name=jenkins-vault-agent
-export pull_secret_name=redhat-registry-io
 export buildconfig_name=${imagestream_name}-build
-oc create imagestream ${imagestream_name} --namespace=${namespace_name}
+export build_pull_secret_name=registry-redhat-io
+oc create imagestream ${imagestream_name} --namespace=${build_namespace_name}
 
 # create BuildConfig and build image
 function process_manifest() {
@@ -15,13 +17,15 @@ function process_manifest() {
   local dir_path=${2:-$(cd $(dirname ${BASH_SOURCE[-1]}) && pwd)}
 
   envsubst < ${dir_path}/${manifest_name}.yaml.tpl > ${dir_path}/${manifest_name}.yaml
-  oc apply --filename ${dir_path}/${manifest_name}.yaml --namespace=${namespace_name}
+  oc apply --filename ${dir_path}/${manifest_name}.yaml --namespace=${build_namespace_name}
   rm ${dir_path}/${manifest_name}.yaml
 }
-process_manifest "build"
-oc start-build  ${buildconfig_name} --namespace=${namespace_name} --from-dir=${root_dir} --no-cache --follow
+pull_secret_name=${build_pull_secret_name} process_manifest "build"
+oc start-build  ${buildconfig_name} --namespace=${build_namespace_name} --from-dir=${root_dir} --no-cache --follow
 
 # apply configmap to current and default namespaces
 # this makes the agent available
-oc process -f ${root_dir}/hack/agent-configmap-template.yaml | oc apply -f -
-oc process -f ${root_dir}/hack/agent-configmap-template.yaml -n default | oc apply -f -
+oc process -f ${root_dir}/hack/agent-configmap-template.yaml \
+  -p IMAGE_URL=image-registry.openshift-image-registry.svc:5000/${build_namespace_name}/${imagestream_name} \
+  -p IMAGE_PULL_SECRET=openshift-internal-registry | \
+    oc apply --namespace ${agent_namespace_name} -f -
